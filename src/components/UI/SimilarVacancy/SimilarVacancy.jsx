92e7ce60e3b275ef8d83fd8e6bf8add7fe7fa6ac
lae1965 <lae1965@yandex.ru>
2024-08-04 02:47:15 +0300

    task-done

--- src/components/UI/SimilarVacancy/SimilarVacancy.jsx
+++ src/components/UI/SimilarVacancy/SimilarVacancy.jsx
@@ -4,23 +4,27 @@ import { useEffect, useState } from 'react';
 import { SekeletonCard } from '../VacancyList/VacancyBlock/VacancyCard/SekeletonCard';
 import { VacancyCard } from '../VacancyList/VacancyBlock/VacancyCard/VacancyCard';
 
-import styles from './SimilarVacancy.module.css'
+import styles from './SimilarVacancy.module.css';
 
 export const SimilarVacancy = () => {
   const [similarVacancies, setSimilarVacancies] = useState([]);
   const [similarPage, setSimilarPage] = useState(1);
 
-  const { vacancyDetail, fetchSimilarVacancy, similaryPages, loadingSimilary } = useDetailVacancyStore();
+  const { vacancyDetail, fetchSimilarVacancy, similaryPages, loadingSimilary } =
+    useDetailVacancyStore();
 
   useEffect(() => {
-    if (!vacancyDetail.id) return
+    if (!vacancyDetail.id) return;
     const fetchData = async () => {
-      const loadVacancy = await fetchSimilarVacancy(vacancyDetail.id, similarPage - 1)
+      const loadVacancy = await fetchSimilarVacancy(
+        vacancyDetail.id,
+        similarPage - 1
+      );
       if (similarPage === 1) setSimilarVacancies(loadVacancy);
       else setSimilarVacancies([...similarVacancies, ...loadVacancy]);
     };
-    fetchData()
-  }, [similarPage, vacancyDetail.id])
+    fetchData();
+  }, [similarPage, vacancyDetail.id]);
   return (
     <>
       <h3 className={styles.moreVacancy}>Похожие вакансии</h3>
@@ -35,20 +39,22 @@ export const SimilarVacancy = () => {
           />
         ))}
       </ul>
-      {loadingSimilary &&
+      {loadingSimilary && (
         <ul className={styles.listSimilar}>
           {Array.from({ length: LIMIT_CAR_FOR_SIMILARY }).map((_, index) => (
             <SekeletonCard key={index} />
           ))}
         </ul>
-      }
+      )}
       <div className={styles.btnWrapper}>
         <button
-          className={`${styles.btn} ${styles.lodaMore}`}
+          className={styles.btn}
           disabled={similarPage - 1 >= similaryPages}
-          onClick={() => setSimilarPage(similarPage => similarPage + 1)}>Показать еще</button>
+          onClick={() => setSimilarPage((similarPage) => similarPage + 1)}
+        >
+          Показать еще
+        </button>
       </div>
     </>
-  )
-}
-
+  );
+};
