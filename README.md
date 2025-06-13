41a7ccdf046245bb35e2ebd8b487bba85a389a23
Michael <machapurin@icloud.com>
2025-06-13 11:30:03 +0300

    add link to deploy in readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # CareerApp
 
 Командный проект, клон HH
+
+[деплой](https://career-app-29j3.vercel.app/)
--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -6,5 +6,4 @@ export default defineConfig({
   html: {
     title: "Career App",
   },
-  outputDir: "public",
 });
